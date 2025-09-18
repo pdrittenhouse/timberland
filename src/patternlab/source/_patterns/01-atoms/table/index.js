ac8785f83972e10fd6e0f892f9f9ab9ef91aa1bb
Patrick Rittenhouse <pdee999@gmail.com>
2025-09-18 13:25:02 -0400

    adding table pattern and field group

---
+++ src/patternlab/source/_patterns/01-atoms/table/index.js
@@ -0,0 +1,16 @@
+// Table pattern JavaScript
+// Add any table-specific JavaScript functionality here
+
+export default class Table {
+  constructor(element) {
+    this.element = element;
+    this.init();
+  }
+
+  init() {
+    // Initialize table functionality
+    // Example: Add sorting, filtering, or other interactive features
+  }
+
+  // Add table methods here
+}
